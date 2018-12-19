8e4da1461fa5574a2ab475e8b11de23eb8773705
ZHU YIFAN <simba23333@gmail.com>
2018-12-19 09:26:28 +0800

    Update Bear.java

--- All projects/Module 4 - Animal Kingdom/Bear.java
+++ All projects/Module 4 - Animal Kingdom/Bear.java
@@ -1,34 +1,38 @@
 import java.awt.*;
 
-public class Bear extends Critter{
-    boolean polar;
+public class Giant extends Critter{
     int moves;
+    String s;
 
-    public Bear(boolean polar){
-        this.polar = polar;
+    public Giant(){
         moves = 0;
     }
 
-    // Color.WHITE for a polar bear (when polar is true), Color.BLACK otherwise (when polar is false)
-    public Color getColor(){
-        if (polar){
-            return Color.WHITE;
-        } else {
-            return Color.BLACK;
-        }
+    //  Color.GRAY
+    public Color getColor() {
+        return Color.GRAY;
     }
 
-    // Should alternate on each different move between a slash character (/) and a backslash character ()
-    // starting with a slash.
+    // "fee" for 6 moves, then "fie" for 6 moves, then "foe" for 6 moves, then "fum" for 6 moves, then
+    // repeat.
     public String toString() {
-        if (moves % 2 == 0){
-            return "/";
+        if (moves % 6 == 0) {
+            if (moves % 24 == 0) {
+                s = "fee";
+            } else if (moves % 24 == 6) {
+                s = "fie";
+            } else if (moves % 24 == 12){
+                s = "foe";
+            } else {
+                s = "fum";
+            }
+            return s;
         } else {
-            return "\\";
+            return s;
         }
     }
 
-    // always infect if an enemy is in front, otherwise hop if possible, otherwise turn left.
+    //  always infect if an enemy is in front, otherwise hop if possible, otherwise turn right.
     public Action getMove(CritterInfo info) {
         moves++;
 
@@ -37,7 +41,7 @@ public class Bear extends Critter{
         } else if (info.getFront() == Neighbor.EMPTY){
             return Action.HOP;
         } else {
-            return Action.LEFT;
+            return Action.RIGHT;
         }
     }
 }
