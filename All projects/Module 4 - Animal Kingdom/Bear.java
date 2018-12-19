85e1c0d81d5861b61e563d6cb66a93ca72cb513a
ZHU YIFAN <simba23333@gmail.com>
2018-12-19 09:29:48 +0800

    Update Bear.java

--- All projects/Module 4 - Animal Kingdom/Bear.java
+++ All projects/Module 4 - Animal Kingdom/Bear.java
@@ -1,38 +1,34 @@
 import java.awt.*;
 
-public class Giant extends Critter{
+public class Bear extends Critter{
+    boolean polar;
     int moves;
-    String s;
 
-    public Giant(){
+    public Bear(boolean polar){
+        this.polar = polar;
         moves = 0;
     }
 
-    //  Color.GRAY
-    public Color getColor() {
-        return Color.GRAY;
-    }
-
-    // "fee" for 6 moves, then "fie" for 6 moves, then "foe" for 6 moves, then "fum" for 6 moves, then
-    // repeat.
-    public String toString() {
-        if (moves % 6 == 0) {
-            if (moves % 24 == 0) {
-                s = "fee";
-            } else if (moves % 24 == 6) {
-                s = "fie";
-            } else if (moves % 24 == 12){
-                s = "foe";
-            } else {
-                s = "fum";
-            }
-            return s;
+    // Color.WHITE for a polar bear (when polar is true), Color.BLACK otherwise (when polar is false)
+    public Color getColor(){
+        if (polar){
+            return Color.WHITE;
         } else {
-            return s;
+            return Color.BLACK;
         }
     }
 
-    //  always infect if an enemy is in front, otherwise hop if possible, otherwise turn right.
+    // Should alternate on each different move between a slash character (/) and a backslash character ()
+    // starting with a slash.
+    public String toString() {
+        if (moves % 2 == 0){
+            return "/";
+        } else {
+            return "\\";
+        }
+    }
+
+    // always infect if an enemy is in front, otherwise hop if possible, otherwise turn left.
     public Action getMove(CritterInfo info) {
         moves++;
 
@@ -41,7 +37,7 @@ public class Giant extends Critter{
         } else if (info.getFront() == Neighbor.EMPTY){
             return Action.HOP;
         } else {
-            return Action.RIGHT;
+            return Action.LEFT;
         }
     }
 }
