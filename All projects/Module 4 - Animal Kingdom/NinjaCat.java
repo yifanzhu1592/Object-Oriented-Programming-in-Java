ac9431d0f47313808cd34ce30d1f7f6031a30676
ZHU YIFAN <simba23333@gmail.com>
2018-12-19 09:20:26 +0800

    Update NinjaCat.java

--- All projects/Module 4 - Animal Kingdom/NinjaCat.java
+++ All projects/Module 4 - Animal Kingdom/NinjaCat.java
@@ -5,14 +5,17 @@ public class NinjaCat extends Critter{
 
     }
 
+    //  Color.PINK
     public Color getColor(){
         return Color.pink;
     }
 
+    //  "Meow"
     public String toString() {
         return "Meow";
     }
 
+    //  always infect whether there is an enemy facing you in front of you, otherwise hop if possible, otherwise turn left.
     public Action getMove(CritterInfo info) {
         if (info.frontThreat()) {
             return Action.INFECT;
